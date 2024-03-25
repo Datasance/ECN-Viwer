14d942c95e2c98f846f7b3933c471b3ca55805ec
Emirhan Durmuş <emirhan.durmus@datasance.com>
2024-03-25 21:13:56 +0300

    keycloak init config and publish script edited

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 # Import our helper functions
 . scripts/utils.sh
 
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 prettyTitle "Datasance ioFog ECN Viewer Packaging"
 echoInfo "Beginning packaging process"
--- src/Keycloak/keycloakConfig.js
+++ src/Keycloak/keycloakConfig.js
@@ -1,14 +1,15 @@
 import Keycloak from 'keycloak-js'
 const controllerJson = window.controllerConfig
 
-const initOptions = {
-  url: `${controllerJson.keycloakURL}`,
-  realm: controllerJson.keycloakRealm,
-  clientId: controllerJson.keycloakClientid,
+debugger
+let initOptions = {
+  url: controllerJson?.keycloakURL,
+  realm: controllerJson?.keycloakRealm,
+  clientId: controllerJson?.keycloakClientid,
   onLoad: 'login-required',
-  KeycloakResponseType: 'code'
-}
+  KeycloakResponseType: "code",
+};
 
-const keycloak = new Keycloak(initOptions)
+const keycloak = new Keycloak(initOptions);
 
 export default keycloak
