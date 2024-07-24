ff8266fd15627237d880d29b953458b5194d62f2
Alpaslan DOGAN <alpaslanndogan@hotmail.com>
2024-07-24 11:09:36 +0300

    bugfix for app yaml

--- src/ECNViewer/ApplicationDetails/index.js
+++ src/ECNViewer/ApplicationDetails/index.js
@@ -138,10 +138,10 @@ export default function ApplicationDetails({
             name: (reducedAgents.byUUID[m.iofogUuid] || { name: "__UNKNOWN__" })
               .name,
           },
-          runAsUser: app?.runAsUser,
-          platform: app?.platform,
-          runtime: app?.runtime,
-          cdiDevices: app?.cdiDevices !== undefined ? app?.cdiDevices : [],
+          runAsUser: m?.runAsUser,
+          platform: m?.platform,
+          runtime: m?.runtime,
+          cdiDevices: m?.cdiDevices !== undefined ? m?.cdiDevices : [],
           images: m.images.reduce(
             (acc, image) => {
               switch (image.fogTypeId) {
