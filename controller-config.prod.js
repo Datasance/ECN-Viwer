5c118ce62a05661a9d085b3169a66c65be9724bb
Emirhan Durmuş <emirhan.durmus@datasance.com>
2025-07-03 22:16:17 +0300

    fix

--- controller-config.prod.js
+++ controller-config.prod.js
@@ -1,21 +1,8 @@
-// window.controllerConfig = {
-//   port: '51121',
-//   refresh: 3000,
-//   user: {
-//     email: '',
-//     password: ''
-//   }
-// }
-
 window.controllerConfig = {
-  url: 'http://40.69.193.23:51121',
   port: '51121',
   refresh: 3000,
   user: {
     email: '',
     password: ''
-  },
-  keycloakURL: 'https://auth0.demo.datasance.com/',
-  keycloakRealm: 'datasance',
-  keycloakClientid: 'ecn-viewer'
+  }
 }
