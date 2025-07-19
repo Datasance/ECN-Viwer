8fbe8e6df9ef458668e13fe66074bbb04292868a
Emirhan Durmuş <emirhan.durmus@datasance.com>
2025-07-19 11:17:53 +0300

    release prep

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 # Import our helper functions
 . scripts/utils.sh
 
-VERSION="v1.0.0-beta9"
+VERSION="v1.0.0-beta10"
 
 prettyTitle "Datasance ioFog ECN Viewer Packaging"
 echoInfo "Beginning packaging process"
